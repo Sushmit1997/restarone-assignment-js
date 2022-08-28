7e491a8337632d5271c0c7facd4fb8fc53dead4b
Sushmit <sushmit.rajaure@gmail.com>
2022-08-28 21:01:14 +0545

    Variable names updated, readme updated

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # restarone-assignment-js
+
+Deployed github page for this assignment can be found here: https://sushmit1997.github.io/restarone-assignment-js/
--- index.js
+++ index.js
@@ -24,18 +24,18 @@ function addRowHandlers() {
     var currentRow = table.rows[i];
     var createClickHandler = function (row) {
       return function () {
-        var cell = row.getElementsByTagName("td")[1];
         var initialCell = row.getElementsByTagName("td")[0];
+        var nameCell = row.getElementsByTagName("td")[1];
         var companyCell = row.getElementsByTagName("td")[2];
         var workEmailCell = row.getElementsByTagName("td")[4];
 
         var initial = initialCell.innerText;
-        var id = cell.innerHTML;
+        var name = nameCell.innerHTML;
         var companyName = companyCell.innerText;
         var workEmail = workEmailCell.innerText;
 
-        document.querySelector(".userDetailName").innerHTML = id;
-        document.querySelector(".userDetailNameTop").innerHTML = id;
+        document.querySelector(".userDetailName").innerHTML = name;
+        document.querySelector(".userDetailNameTop").innerHTML = name;
         document.querySelector(".rowDetailUserInitials").innerHTML = initial;
         document.querySelector(".userDetailCompanyTop").innerHTML = companyName;
         document.querySelector(".userDetailCompany").innerHTML = companyName;
